2fddec137d68cc330a6ebb30481fcadcadffff9c
LynAs <szlynas@gmail.com>
2016-03-15 10:30:08 +0600

    init commit

---
+++ src/main/java/com/lynas/config/JwtAuthenticationSuccessHandler.java
@@ -0,0 +1,19 @@
+package com.lynas.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by LynAs on 15-Mar-16
+ */
+public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
